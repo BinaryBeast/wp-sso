84360ae9cf4f08d28162a09dfd9f5ec7c4edd7e2
Brandon Simmons <binary@apple.com>
2015-08-28 02:30:37 -0700

    Minor refactoring

--- binarybeast-sso.php
+++ binarybeast-sso.php
@@ -9,3 +9,5 @@ Author: Brandon Simmons <brandon@binarybeast.com>
 Author URI: http://binarybeast.com
 License: A "Slug" license name e.g. GPL2
 */
+
+defined( 'ABSPATH' ) or die( '403 Access Denied' );
---
+++ readme.txt
@@ -0,0 +1,5 @@
+=== binarybeast-sso ===
+Requires at least: 4.3
+Tested up to: 4.3
+
+Allows user authentication to be handled by the BinaryBeast API
