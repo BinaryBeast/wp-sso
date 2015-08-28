b51d76d671ed9d653e819bf72b369f395f297e14
Brandon Simmons <binary@apple.com>
2015-08-28 02:18:36 -0700

    Initial setup

---
+++ BB-w-p-login.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: BinaryBeast SSO
+Plugin URI: https://github.com/BinaryBeast/wp-sso
+Description: Allows user authentication to be handled by the BinaryBeast API
+Version: 0.0.1
+Author: Brandon Simmons <brandon@binarybeast.com>
+Author URI: http://binarybeast.com
+License: A "Slug" license name e.g. GPL2
+*/
